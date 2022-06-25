04432b1626e56d0cdc88d6d3b08928a2cd9f58d4
Gourav Singh <gouravsingh510gmail.com@Gouravs-MacBook-Air.local>
2022-06-25 20:51:04 +0530

    update

--- vars/deploy_app.groovy
+++ vars/deploy_app.groovy
@@ -2,7 +2,7 @@ import buildStatus
 import groovy.json.JsonBuilder
 
 def call(){
-    echo env.json
+    echo "${env.json}"
     echo 'abc'
     echo json
   //buildStatuses << new buildStatus ( test_name: 'pytest', status: true )
